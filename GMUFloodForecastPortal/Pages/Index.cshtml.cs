087b3867a6f76fbccf5e7838644bfa2df939329e
Bai He <baihe@microsoft.com>
2018-12-28 13:23:09 -0800

    updates

--- GMUFloodForecastPortal/Pages/Index.cshtml.cs
+++ GMUFloodForecastPortal/Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -9,9 +11,27 @@ namespace GMUFloodForecastPortal.Pages
 {
     public class IndexModel : PageModel
     {
+        public List<string> KmlFiles = new List<string>();
+
         public void OnGet()
         {
+        /*
+            string serverUrl = FileServerUtil.GetFileServerUrl("13.78.149.101", 21, "subset");
+            FtpWebRequest ftpRequest = (FtpWebRequest)WebRequest.Create(serverUrl);
+            ftpRequest.Method = WebRequestMethods.Ftp.ListDirectory;
+            ftpRequest.Credentials = FileServerUtil.GetFileServerCredential();
 
+            FtpWebResponse ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+
+            string[] directoryOrFileNames;
+            using (Stream responseStream = ftpResponse.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream);
+                string result = reader.ReadToEnd();
+                directoryOrFileNames = String.IsNullOrEmpty(result) ? null : result.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                KmlFiles = directoryOrFileNames.ToList();
+            }
+            */
         }
     }
 }
