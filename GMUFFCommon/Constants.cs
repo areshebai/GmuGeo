5e0f5f1ddaaac289479a5141ac8eebf0bfd80f4d
Bai He <baihe@microsoft.com>
2019-01-16 16:26:31 -0800

    Add constants

---
+++ GMUFFCommon/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GMUFFCommon
+{
+    public static class Constants
+    {
+        public const string Satellite_SuomiNPP_Name = "Suomi-NPP";
+        public const string Satellite_NOAA20_Name = "NOAA-20";
+        public const string Satellite_GOES16_Name = "GOES-16";
+    }
+}
